f371f01ffd31cdc3a17f1a4d092f976b2644118f
JobLessGod <iamjoblessgod@gmail.com>
2025-03-29 19:09:10 +0545

    Comment out SplashScreen and font loading logic in layout component

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,10 @@
 import { Stack, useRouter, useSegments } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
+// import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 import { useAuthStore } from "../store/authStore";
 import { useEffect } from "react";
 
@@ -18,13 +18,13 @@ export default function RootLayout() {
     checkAuth();
   }, []);
 
-  const [fontsLoaded] = useFonts({
-    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMonoNL-Medium.ttf"),
-  });
+  // const [fontsLoaded] = useFonts({
+  //   "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMonoNL-Medium.ttf"),
+  // });
 
-  useEffect(() => {
-    if (fontsLoaded) SplashScreen.hideAsync();
-  }, [fontsLoaded]);
+  // useEffect(() => {
+  //   if (fontsLoaded) SplashScreen.hideAsync();
+  // }, [fontsLoaded]);
 
   useEffect(() => {
     const isThisAuthScreen = segments[0] === "(auth)";
